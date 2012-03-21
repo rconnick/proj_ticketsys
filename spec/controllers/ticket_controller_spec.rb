fb299c08cb37afc484cab3a2d81fb4677b137689
Robert <rconnick@berkeley.edu>
2012-03-21 07:57:57 -0700

    Added some controller tests.

--- spec/controllers/ticket_controller_spec.rb
+++ spec/controllers/ticket_controller_spec.rb
@@ -21,5 +21,37 @@ describe TicketController do
     
   end
   
+  describe 'logging in' do
+    it 'should call the User model method to find a user by name' do
+      User.should_receive(:find_by_username)
+      post :login
+    end
+    describe 'using an invalid username/password' do
+      it 'should redirect to the index with an error' do
+        admin = mock('User')
+        admin.stub(:password) {'right'}
+        User.should_receive(:find_by_username).with('admin').and_return(admin)
+        post :login, :username => 'admin', :password => 'wrong'
+        response.should redirect_to :action => 'index', :notice => 'Invalid Username/Password'
+      end
+    end
+    describe 'using a valid username/password' do
+      it 'should redirect to the index with no error' do
+        admin = mock('User')
+        admin.stub(:password) {'right'}
+        admin.stub(:method_missing)
+        User.should_receive(:find_by_username).with('admin').and_return(admin)
+        post :login, :username => 'admin', :password => 'right'
+        response.should redirect_to :action => 'index', :notice => nil
+      end
+    end
+  end
+  describe 'viewing the main page' do
+    it 'should call the where method of the Ticket model' do
+      Ticket.should_receive(:where).exactly(4).times
+      get :index
+    end
+  end
+  
   
 end
